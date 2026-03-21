552465eeef003319e5ed31882dbadf6cd134274e
Joel Huckle <joel.huckle022024@my.ntu.ac.uk>
2026-03-21 19:10:18 +0000

    Initial Commit

---
+++ Flowdoro/FlowdoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowdoroApp.swift
+//  Flowdoro
+//
+//  Created by Joel Huckle on 21/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlowdoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
